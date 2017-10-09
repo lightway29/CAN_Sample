88cd9a880b8cb4417ac3a4b1f2e056c867f172a7
Lightway <lightway29@gmail.com>
2017-10-09 13:07:41 +0530

    CAN interrupt flag issue fixed by using the volatile qualifier

--- src/main.cpp
+++ src/main.cpp
@@ -1,66 +1,34 @@
 #include "mbed.h"
 
-Ticker ticker;
 DigitalOut led1(LED1);
-DigitalOut led2(LED2);
-
-// CAN can(p30, p29);
 CAN can(p9, p10);
-
-char counter = 0;
-bool msg_rx_flag = 0;
-char sendPayload[8] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
-
-void send() {
-
-  int rc =
-      can.write(CANMessage(0x00000000, sendPayload, 8, CANData, CANExtended));
-  printf("[-INFO-] CAN Message sent. [rc] %d \n\r", rc);
-}
+CANMessage msg;
+volatile bool msg_rx_flag = 0;
 
 void can_interrupt() {
-  can.attach(NULL); // Disable the interrupt - the application must re-anable
-                    // the interrupt after it has acted upon it
-  msg_rx_flag = 1;  // Set a 'Flag' to say that there is an interrupt that needs
-                    // to be processed
-}
-
-void blink() {
-  led1 = 1;
-  wait(1);
-  led1 = 0;
-  wait(1);
+  can.attach(NULL);
+  msg_rx_flag = 1;
 }
 
 int main() {
   printf("[DEBUG LOG] Main loop started. \n\r");
-  // ticker.attach(&send, 3);
-  CANMessage msg;
 
   can.mode(CAN::Normal);
-  can.frequency(500000); // Initialise mbed CAN with a baudrate of 500 kbps
+  can.frequency(100000);  // Initialise mbed CAN with a baudrate of 100 kbps
+  can.attach(callback(&can_interrupt), CAN::RxIrq);
 
   while (1) {
-
-    msg_rx_flag = 1; // This is due to the mbed bug
-                     // https://github.com/ARMmbed/mbed-os/issues/56 once a
-                     // proper fix this can be removed.
-
-    // if(msg_rx_flag == 1 || true){
-
-    if (can.read(msg)) {
-      printf("Message received: %d\n\r", msg.data[0]);
-      led2 = !led2;
-    } else {
-      // printf("Message Not loaded \n\r");
+    if (msg_rx_flag == 1) {
+      if (can.read(msg) == 1) {
+        printf(
+            "Received CAN message | id: %x, data: [0] %x, [1] %x, "
+            "[2] %x, [3] %x, [4] %x, [5] %x, [6] %x, [7] %x \r\n",
+            msg.id, msg.data[0], msg.data[1], msg.data[2], msg.data[3], msg.data[4], msg.data[5], msg.data[6],
+            msg.data[7]);
+      }
+      led1 = !led1;
+      msg_rx_flag = 0;
+      can.attach(callback(&can_interrupt), CAN::RxIrq);
     }
-
-    // can.attach(&can_interrupt,CAN::RxIrq);
-    msg_rx_flag = 0;
-    // wait(0.2);
-    send();
-    wait(0.2);
-    blink();
-    // }
   }
 }
